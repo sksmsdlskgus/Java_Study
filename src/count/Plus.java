7501f53f26fabac6cdfe5ad38c6781ee56e92818
dksmsdlskgus <lnh03280@g.eulji.ac.kr>
2023-12-28 17:25:59 +0900

    just_code2

---
+++ src/count/Plus.java
@@ -0,0 +1,27 @@
+package count;
+
+public class Plus {
+    public class Solution { // 각 자릿수 합
+        public int solution(int n) {
+            int answer = 0;
+            while (n != 0) {
+                answer += n % 10;
+                n /= 10;
+            }
+
+            return answer;
+        }
+    }
+
+    public class Solution2 {
+        public int solution(int n) {
+            // 정수를 문자열로 변환하여 각 자리의 문자에 대한 스트림을 얻음
+            return String.valueOf(n)
+                    .chars()
+                    // 각 문자를 숫자로 변환하여 매핑
+                    .map(Character::getNumericValue)
+                    // 숫자들의 합을 계산
+                    .sum();
+        }
+    }
+}
