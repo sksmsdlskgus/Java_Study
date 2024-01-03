47bf01fd3dc3531156163610d618e3a63bfc7593
dksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-01-03 18:54:16 +0900

    refactor : numberArray

--- src/count/Array.java
+++ src/count/Array.java
@@ -16,34 +16,24 @@ public class Array {
             return answer;
         }
     }
-//    public static int[] reverseDigits(int n) {
-//        // 결과를 저장할 ArrayList를 생성합니다.
-//        ArrayList<Integer> reversedList = new ArrayList<>();
-//
-//        // 주어진 자연수 n을 뒤집어서 reversedList에 추가합니다.
-//        while (n > 0) {
-//            int digit = n % 10; // 현재 자릿수를 구합니다.
-//            reversedList.add(digit); // 배열에 자릿수를 추가합니다.
-//            n /= 10; // 다음 자릿수를 확인하기 위해 n을 10으로 나눕니다.
-//        }
-//
-//        // ArrayList를 int 배열로 변환합니다.
-//        int[] result = new int[reversedList.size()];
-//        for (int i = 0; i < result.length; i++) {
-//            result[i] = reversedList.get(i);
-//        }
-//
-//        return result;
-//    }
-//
-//    public static void main(String[] args) {
-//        int n = 12345;
-//        int[] reversedArray = reverseDigits(n);
-//
-//        // 결과를 출력합니다.
-//        System.out.print("Reversed Digits: ");
-//        for (int digit : reversedArray) {
-//            System.out.print(digit + " ");
-//        }
-//    }
+
+    class Solution2 {
+        public int[] solution(long n) {
+            String num = Long.toString(n);
+            int length = num.length();
+            int[] answer = new int[length];
+
+            for (int i = 0; i < length; i++) {
+                answer[i] = num.charAt(length - 1 - i) - '0';
+            }
+
+            return answer;
+        }
+    }
+
+    class Solution3 {
+        public int[] solution(long n) {
+            return new StringBuilder().append(n).reverse().chars().map(Character::getNumericValue).toArray();
+        }
+    }
 }
