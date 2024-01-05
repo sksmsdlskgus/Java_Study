efe7c87ecd4b39379388cbf6ff9902a65906bc4f
dksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-01-05 20:10:36 +0900

    refactor: Array_sort

--- src/count/array/Array2.java
+++ src/count/array/Array2.java
@@ -23,5 +23,16 @@ public class Array2 {
             return answer;
         }
     }
+    class Solution2 {
+        public long solution2(long n) {
+            String[] list = String.valueOf(n).split("");
+            Arrays.sort(list);
+
+            StringBuilder sb = new StringBuilder();
+            for (String aList : list) sb.append(aList);
+
+            return Long.parseLong(sb.reverse().toString());
+        }
+    }
 
 }
