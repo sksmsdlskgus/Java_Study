9d968919621826f779bfe528e3f283a93198d33a
dksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-01-08 17:20:50 +0900

    refactor: Scountf_1st 부가설명

--- src/count/Scount/Scount2.java
+++ src/count/Scount/Scount2.java
@@ -26,6 +26,12 @@ public class Scount2 {
 
             // x의 각 자릿수의 합을 구함
             int originalX = x;  // x의 원본 값을 보존하기 위한 변수 주의 !!!
+            //  while 루프에서 x를 10으로 나누어가며 각 자릿수의 합을 계산할 때,
+            //  x가 0이 되기 전까지 루프가 돌게 됨
+            // 그리고 나서 if (originalX % result != 0)에서 원본 값인 originalX를 사용하여
+            //  하샤드 수 여부를 판단
+            // 만약 x의 값을 계속해서 나누어가면,
+            // x의 값이 0이 되어 루프를 빠져나오면서 originalX도 0이 되어버리게 됨
             while(x != 0) {
                 result += x % 10;
                 x /= 10;
