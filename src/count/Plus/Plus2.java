01bf73312a04e8b78cbf5abda9131fa44d5a01c7
dksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-01-09 03:02:01 +0900

    Plus2_1st

--- src/count/Plus/Plus2.java
+++ src/count/Plus/Plus2.java
@@ -5,6 +5,7 @@ public class Plus2 {
         public long solution(int a, int b) {
             //  a와 b 사이에 속한 모든 정수의 합을 리턴
             // a와 b가 같을 경우에는 둘 중 아무 수나 리턴
+            // 예를 들어 a = 3, b = 5인 경우, 3 + 4 + 5 = 12이므로 12를 리턴
             long answer = 0;
             return answer;
         }
