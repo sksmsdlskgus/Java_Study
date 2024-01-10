4409e017bc0a4bdfe3121516d3c5629a4f295c13
dksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-01-11 00:24:12 +0900

    Plus2_3st

--- src/count/Plus/Plus2.java
+++ src/count/Plus/Plus2.java
@@ -13,4 +13,21 @@ public class Plus2 {
             return answer;
         }
     }
+
+    class Solution2 {
+        public long solution2(int a, int b) {
+            // a와 b 사이에 속한 모든 정수의 합을 리턴
+            // a와 b가 같을 경우에는 둘 중 아무 수나 리턴
+            if (a == b) {
+                return a; // Return either a or b if they are equal
+            } else if (a > b) {
+                int temp = a;
+                a = b;
+                b = temp;
+            }
+
+            return (long) (b - a + 1) * (a + b) / 2;
+        }
+    }
+
 }
