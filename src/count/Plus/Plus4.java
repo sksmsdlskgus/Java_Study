f1d43d9b7d5215cfeef6c3b93a69a948f628674b
dksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-01-15 22:03:39 +0900

    Plus4_1st

---
+++ src/count/Plus/Plus4.java
@@ -0,0 +1,29 @@
+package count.Plus;
+
+public class Plus4 {
+    class Solution {
+        public int solution(int num) {
+            // 주어진 수가 1이 될 때까지 반복
+            // 1-1. 입력된 수가 짝수라면 2로 나눕니다.
+            // 1-2. 입력된 수가 홀수라면 3을 곱하고 1을 더합니다.
+            // 2. 결과로 나온 수에 같은 작업을 1이 될 때까지 반복합니다.
+            // 단, 주어진 수가 1인 경우에는 0을 반환
+            // 작업을 500번 반복할 때까지 1이 되지 않는다면 –1을 반환
+            int answer = 0;
+
+            while (num != 1 && answer < 500) {
+                if (num % 2 == 0) {
+                    // 1-1. 입력된 수가 짝수인 경우
+                    num = num / 2;
+                } else {
+                    // 1-2. 입력된 수가 홀수인 경우
+                    num = num * 3 + 1;
+                }
+                answer++; // 반복 횟수를 세기 위해 answer를 1 증가시킵니다.
+
+            }
+            return answer;
+        }
+    }
+
+}
