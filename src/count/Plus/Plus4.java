b5c39572c329049f73a3f7124b0bd760ee3acff6
dksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-01-15 22:25:59 +0900

    Plus4_2st

--- src/count/Plus/Plus4.java
+++ src/count/Plus/Plus4.java
@@ -11,19 +11,27 @@ public class Plus4 {
             // 작업을 500번 반복할 때까지 1이 되지 않는다면 –1을 반환
             int answer = 0;
 
-            while (num != 1 && answer < 500) {
+            while (num != 1) {
                 if (num % 2 == 0) {
                     // 1-1. 입력된 수가 짝수인 경우
                     num = num / 2;
-                } else {
+                } else if(num % 2 == 1) {
                     // 1-2. 입력된 수가 홀수인 경우
                     num = num * 3 + 1;
                 }
-                answer++; // 반복 횟수를 세기 위해 answer를 1 증가시킵니다.
+
+                answer++;// 반복 횟수를 세기 위해 answer를 1 증가시킵니다.
+
+                if(answer>=500){
+                    answer=-1;
+                    break;
+                }
 
             }
             return answer;
+
         }
     }
+
 
 }
