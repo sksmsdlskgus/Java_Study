72b6d0a3d74cd4778b30ab3e2c100dff3639c23d
dksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-01-20 17:15:54 +0900

    Array

---
+++ src/count/array/Array3.java
@@ -0,0 +1,12 @@
+package count.array;
+
+public class Array3 {
+    class Solution {
+        public int[] solution(int[] arr, int divisor) {
+            // array각 요소중 divisor로 나누어 떨어지는 값을 오름차순으로 정렬한 배열 반환
+            //divisor로 나누어 떨어지는 요소가 없다면 배열에 -1을 담아 반환
+            int[] answer = {};
+            return answer;
+        }
+    }
+}
